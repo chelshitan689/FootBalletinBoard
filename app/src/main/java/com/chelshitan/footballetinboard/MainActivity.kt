530a42b41bd6af31bafd63c60d716358d8c79a98
chelshitan689 <81636631+chelshitan689@users.noreply.github.com>
2023-08-30 23:32:39 +0900

    サンプルコピペで最低限の掲示板は完成

--- app/src/main/java/com/chelshitan/footballetinboard/MainActivity.kt
+++ app/src/main/java/com/chelshitan/footballetinboard/MainActivity.kt
@@ -2,6 +2,8 @@ package com.chelshitan.footballetinboard
 import Task
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.chelshitan.footballetinboard.databinding.ActivityMainBinding
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -21,16 +23,56 @@ class MainActivity : AppCompatActivity() {
         // Firestoreをインスタンス化
         val db = Firebase.firestore
 
-        // Taskをインスタンス化
-        val task = Task(
-            "test","test",)
-        db.collection("test")
-            .add(task)
-            .addOnSuccessListener { documentReference ->
-                //Log.d(ADD_TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+        // ボタンを押したときの処理
+        binding.addButton.setOnClickListener {
+
+            // Taskをインスタンス化
+            val task = Task(
+                title = binding.titleEditText.text.toString(),
+            )
+
+            db.collection("tasks")
+                .add(task)
+                .addOnSuccessListener { documentReference ->
+                    //Log.d(ADD_TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+                }
+                .addOnFailureListener { e ->
+                    //Log.w(ADD_TAG, "Error adding document", e)
+                }
+        }
+
+        // RecyclerViewの設定
+        val taskAdapter = TaskAdapter()
+        binding.recyclerView.adapter = taskAdapter
+        binding.recyclerView.layoutManager =
+            LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
+        // アプリ起動時に、保存されているデータを取得する
+        db.collection("tasks")
+            .get()
+            .addOnSuccessListener { tasks ->
+                val taskList = ArrayList<Task>()
+                tasks.forEach { taskList.add(it.toObject(Task::class.java)) }
+                taskAdapter.submitList(taskList)
             }
-            .addOnFailureListener { e ->
-                //Log.w(ADD_TAG, "Error adding document", e)
+            .addOnFailureListener { exception ->
+                //Log.d(READ_TAG, "Error getting documents: ", exception)
             }
+
+        // データの変更をリアルタイムでアプリに反映する
+        db.collection("tasks").addSnapshotListener { tasks, e ->
+            if (e != null) {
+                //Log.w(READ_TAG, "Listen failed.", e)
+                return@addSnapshotListener
+            }
+
+            if (tasks != null) {
+                val taskList = ArrayList<Task>()
+                tasks.forEach { taskList.add(it.toObject(Task::class.java)) }
+                taskAdapter.submitList(taskList)
+            } else {
+                //Log.d(READ_TAG, "Current data: null")
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/chelshitan/footballetinboard/TaskAdapter.kt
@@ -0,0 +1,42 @@
+package com.chelshitan.footballetinboard
+
+import Task
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.chelshitan.footballetinboard.databinding.TaskListItemBinding
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class TaskAdapter : ListAdapter<Task, TaskViewHolder>(diffUtilItemCallback) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+        val view = TaskListItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return TaskViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+}
+
+class TaskViewHolder(
+    private val binding: TaskListItemBinding
+) : RecyclerView.ViewHolder(binding.root) {
+    fun bind(task: Task) {
+        binding.titleTextView.text = task.title
+        binding.dateTextView.text =
+            SimpleDateFormat("yyyy/MM/dd HH:mm:ss", Locale.JAPANESE).format(task.createdAt)
+    }
+}
+
+private val diffUtilItemCallback = object : DiffUtil.ItemCallback<Task>() {
+    override fun areContentsTheSame(oldItem: Task, newItem: Task): Boolean {
+        return oldItem == newItem
+    }
+
+    override fun areItemsTheSame(oldItem: Task, newItem: Task): Boolean {
+        return oldItem.id == newItem.id
+    }
+}
